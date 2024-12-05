43c2e65233c43b2cae679365c526e5a38fa3a13c
ALDARUSSS <2012aldarov@gmail.com>
2024-12-05 11:48:29 +0800

    my first script
    sadf

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //слева от меня сидит хомячок
+        print("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
